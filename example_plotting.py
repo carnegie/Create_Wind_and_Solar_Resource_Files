fb22fd3f7c2419acf11ed8389affe27a6fa9732d
Tyler Ruggles <tylerhruggles@gmail.com>
2020-06-15 15:20:50 -0700

    Plot a shape file we have available, Western IC

--- example_plotting.py
+++ example_plotting.py
@@ -2,12 +2,22 @@
 
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
+from cartopy.io.shapereader import Reader
+from cartopy.feature import ShapelyFeature
 from matplotlib import pyplot as plt
+
+
 #from matplotlib.colors import ListedColormap, LinearSegmentedColormap
-ax1 = plt.subplot(111, projection = ccrs.PlateCarree())
-ax1.set_global()
-ax1.add_feature(cfeature.COASTLINE)
-ax1.pcolormesh( lon, lat, test1, transform=ccrs.PlateCarree() )
-#ax1.set_extent([-150,-50, 10, 80])
+ax = plt.subplot(111, projection = ccrs.PlateCarree())
+ax.set_global()
+ax.add_feature(cfeature.COASTLINE)
+
+# Plot western interconnect
+fname = 'data/US_Interconnects/WesternInterconnect.shp'
+shape_feature = ShapelyFeature(Reader(fname).geometries(),
+                                ccrs.PlateCarree(), edgecolor='black')
+ax.add_feature(shape_feature, facecolor='blue')
+
+#ax.pcolormesh( lon, lat, test1, transform=ccrs.PlateCarree() )
+#ax.set_extent([-150,-50, 10, 80])
 plt.show()
-plt.clf()
