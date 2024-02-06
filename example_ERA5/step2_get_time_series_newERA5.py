af78285f41ccc6321b45a39b238b6e491a27caf4
Jackie Dowling <jdowling@login-srcf.calc.carnegiescience.edu>
2024-02-05 19:55:35 -0800

    asking for help

--- example_ERA5/step2_get_time_series_newERA5.py
+++ example_ERA5/step2_get_time_series_newERA5.py
@@ -17,6 +17,10 @@ scf_or_wcf = 'scf' if len(sys.argv) <= 5 else sys.argv[5]
 source = 'Nan' if len(sys.argv) <= 6 else sys.argv[6]
 mask = 'Nan' if len(sys.argv) <= 7 else sys.argv[7]
 
+#landsea files:
+ERA5path = '/carnegie/nobackup/scratch/jdowling/Create_Wind_and_Solar_Resource_Files/example_ERA5/data/ERA5_landsea_mask.nc'
+MERRA2path = '/carnegie/nobackup/scratch/jdowling/Create_Wind_and_Solar_Resource_Files/example_ERA5/data/land_sea_mask_merra.nc4'
+
 if source == 'MERRA_2': 
     print ('Dealing with ERA5 only')
     stop
@@ -26,17 +30,20 @@ if source == 'ERA5':
         print ('Dealing only ERA5 with ERA5 mask only')
         stop 
     if mask == 'ERA5':
-        data_patch = '/groups/carnegie_poc/leiduan_memex/lustre_scratch/MERRA2_data/ERA5/ERA5_CF_Data/orginal_resolution/'
+        # data_patch = '/groups/carnegie_poc/leiduan_memex/lustre_scratch/MERRA2_data/ERA5/ERA5_CF_Data/orginal_resolution/'
+        data_patch = '/carnegie/nobackup/scratch/jdowling/CFs/ERA5/'
         app = 'SolarCFs_ERA5_' if scf_or_wcf == 'scf' else 'WindCFs_ERA5_'
         pre = 's' if scf_or_wcf == 'scf' else 'w'
-        f_mask = cdms.open('/groups/carnegie_poc/leiduan_memex/lustre_scratch/MERRA2_data/ERA5_landsea_mask.nc')
+        # f_mask = cdms.open('/groups/carnegie_poc/leiduan_memex/lustre_scratch/MERRA2_data/ERA5_landsea_mask.nc')
+        f_mask = cdms.open(ERA5path)
         v = f_mask('lsm', squeeze=1)[0]
         lat = v.getAxis(0)
         lon = v.getAxis(1)
         f_mask.close() 
         case_name = [] 
         for month_idx in np.arange(1, 13, 1):
-            case_name.append(app + str(year) + '_' + str(month_idx) + '_Org.nc')
+            # case_name.append(app + str(year) + '_' + str(month_idx) + '_Org.nc')
+            case_name.append(app + str(year) + '_' + str(month_idx) + '.nc')
         # fm = cdms.open('recreateWrong_selected_USmask_outfile_ERA5.nc')
         fm = cdms.open('step1p2_selected_USmask_outfile_ERA5.nc')
 
---
+++ getclimatedata.py
@@ -0,0 +1,242 @@
+#When using MERRA-2:
+fmask = cdms.open('example_ERA5/data/ERA5_landsea_mask.nc')
+
+#When using ERA-5:
+fmask = cdms.open('example_ERA5/data/ERA5_landsea_mask.nc')
+
+
+#/carnegie/nobackup/scratch/Climate_Energy_Lab
+
+
+def main():
+
+    ERA5path = '/carnegie/nobackup/scratch/jdowling/CFs/ERA5'
+    MERRA2path = '/carnegie/nobackup/scratch/jdowling/CFs/MERRA2'
+
+    run_step1p1()
+    run_step1p2()
+
+
+
+
+def run_step1p1():
+    import numpy as np
+    import geopandas as gpd, regionmask
+    import cdms2 as cdms
+    import MV2 as MV
+
+
+    # When using ERA5:
+    fmask = cdms.open('data/ERA5_landsea_mask.nc')
+    land_mask_tmp = fmask('lsm', squeeze=1)[0]
+    lat = land_mask_tmp.getAxis(0)
+    lon = land_mask_tmp.getAxis(1)
+    fmask.close()
+
+    land_mask_tmp[land_mask_tmp>=0.5] = 1.
+    land_mask_tmp[land_mask_tmp<0.5]  = 0.
+    land_mask = MV.array(MV.masked_equal(land_mask_tmp,0.))
+    land_mask.setAxis(0,lat);      land_mask.setAxis(1,lon)
+    output_file_name = 'selected_CtyMasks_ERA5.nc'
+
+
+
+    # ------------------------------------------------------------------------
+    # """
+    # Get US masks
+    countries = regionmask.defined_regions.natural_earth_v5_0_0.countries_110
+    mask = np.array(countries.mask(lon, lat, wrap_lon=True))
+    name = countries.names
+
+    ### Create US mask
+    # Exclude Alaska and Hawaii from the US mask 
+    longitudes, latitudes = np.meshgrid(lon, lat)
+    # Alaska
+    alaska_lat_mask = (latitudes >= 51) & (latitudes <= 71)
+    alaska_lon_mask = (longitudes >= 188) & (longitudes <= 230)
+    alaska_combined_mask = alaska_lat_mask & alaska_lon_mask
+    # Hawaii
+    hawaii_lat_mask = (latitudes >= 18.5) & (latitudes <= 20.5)
+    hawaii_lon_mask = (longitudes >= 204) & (longitudes <= 206)
+    hawaii_combined_mask = hawaii_lat_mask & hawaii_lon_mask
+    # Combine the Masks
+    final_combined_mask = alaska_combined_mask | hawaii_combined_mask
+    # Separate US mask 
+    mask[mask!=4] = -9999
+    mask[final_combined_mask==True] = -9999
+    mask_masked = np.ma.masked_equal(mask, -9999) * 0 + 1
+
+    US_mask = MV.array(mask_masked)
+    US_mask.setAxis(0,lat)
+    US_mask.setAxis(1,lon)
+    US_mask.id='mask_US'
+
+    ### Create a new NetCDF file and save the mask
+    g=cdms.open('step1p1_selected_masks_US.nc','w')
+    g.write(US_mask)
+    g.close() 
+    # """
+
+
+
+def run_step1p2():
+    import cdms2 as cdms
+    import MV2 as MV
+    import cdutil
+    import numpy as np
+
+
+
+    # ------------------------------------------------------------------------
+
+    # ERA5
+    fmask = cdms.open('data/ERA5_landsea_mask.nc')
+    land_mask_tmp = fmask('lsm', squeeze=1)[0]
+    lat = land_mask_tmp.getAxis(0)
+    lon = land_mask_tmp.getAxis(1)
+    fmask.close()
+
+    land_mask_tmp[land_mask_tmp>=0.5] = 1.
+    land_mask_tmp[land_mask_tmp<0.5]  = 0.
+    land_mask = MV.array(MV.masked_equal(land_mask_tmp,0.))
+    land_mask.setAxis(0,lat);      land_mask.setAxis(1,lon)
+    ocean_mask = (MV.filled(land_mask_tmp, 0) - 1) * -1
+    ocean_mask = MV.array(MV.masked_equal(ocean_mask,0.)) * 0 + 1
+    ocean_mask.setAxis(0,lat);      ocean_mask.setAxis(1,lon)
+
+    data_path = 'data/solar_wind_annual_data_ERA5/'
+    start_year = 2011
+    n_years = 10
+    scf = np.zeros([len(lat), len(lon)])
+    wcf = np.zeros([len(lat), len(lon)])
+    for i in range(n_years):
+        year = i + start_year
+        # file_name = f'Annual_ERA5_{year}_12.nc'
+        file_name = f'Annual_ERA5_{year}_12_Org.nc'
+        file_open = cdms.open(f'{data_path}{file_name}')
+        scf = scf + file_open('annual_scf')/float(n_years)
+        wcf = wcf + file_open('annual_wcf')/float(n_years)
+        file_open.close()
+
+
+
+    # ------------------------------------------------------------------------
+    def set_axes(var):
+        var_new = MV.array(var)
+        var_new.setAxis(0,lat)
+        var_new.setAxis(1,lon)
+        return var_new
+
+    def select_CF(sCF, wCF, idx, *i):
+        if idx == 1.:
+            masked_sCF = np.ceil(sCF)
+            masked_wCF = np.ceil(wCF)
+            s_avg_tmp = cdutil.averager(sCF,axis='yx')
+            w_avg_tmp = cdutil.averager(wCF,axis='yx')
+        elif idx == 2.:
+            # set threshold;
+            # you can change this values based on needs;
+            s_thresholds = 0.26
+            w_thresholds = 0.26
+            # set grids with values lower than the threshold to 0;
+            sCF[sCF<s_thresholds] = 0.
+            wCF[wCF<w_thresholds] = 0.
+            # mask grids with a value of 0 (grids that are not needed);
+            masked_sCF = set_axes(MV.masked_equal(sCF,0.)*0.+1)
+            masked_wCF = set_axes(MV.masked_equal(wCF,0.)*0.+1)
+            s_avg_tmp = cdutil.averager(masked_sCF*sCF,axis='yx')
+            w_avg_tmp = cdutil.averager(masked_wCF*wCF,axis='yx')
+        elif idx ==3.:
+            # set threshold, top X%;
+            # you can change this values based on needs;
+            p_threshold_s = 0.25
+            p_threshold_w = 0.25
+            # sort all grids, values for grids that are
+            # outside the interested region are set to -1;
+            s_reshape = np.sort(np.array(MV.filled(sCF,-1)).ravel())[::-1]
+            w_reshape = np.sort(np.array(MV.filled(wCF,-1)).ravel())[::-1]
+            # remove grids with values of -1;
+            s_no_minus1 = s_reshape[s_reshape!=-1]
+            w_no_minus1 = w_reshape[w_reshape!=-1]
+            # find the threshold for the top X%   
+            num_s = int(len(s_no_minus1)*p_threshold_s)
+            num_w = int(len(w_no_minus1)*p_threshold_w)
+            s_thresholds = s_no_minus1[num_s-1]
+            w_thresholds = w_no_minus1[num_w-1]
+            # mask grids with a value lower than the derived thresholds (grids that are not needed);
+            masked_sCF = set_axes( MV.masked_less(sCF,s_thresholds)*0.+1 )
+            masked_wCF = set_axes( MV.masked_less(wCF,w_thresholds)*0.+1 )
+            s_avg_tmp = cdutil.averager(masked_sCF*sCF,axis='yx')
+            w_avg_tmp = cdutil.averager(masked_wCF*wCF,axis='yx')
+        print(f"For method {idx}, averaged solar capacity factor for the filtered grids is: {s_avg_tmp}")
+        print(f"For method {idx}, averaged wind capacity factor for the filtered grids is: {w_avg_tmp}")
+        print(f"Selected grid cells solar {np.ceil(masked_sCF).sum()}\nSelected grid cells wind {np.ceil(masked_wCF).sum()}")
+        return masked_sCF, masked_wCF
+
+
+    def redo_loop(sCF, wCF, idx, *i):
+        if idx ==3.:
+            s_reshape = np.sort(np.array(MV.filled(sCF,-1)).ravel())[::-1]
+            w_reshape = np.sort(np.array(MV.filled(wCF,-1)).ravel())[::-1]
+            s_no_minus1 = s_reshape[s_reshape!=-1]
+            w_no_minus1 = w_reshape[w_reshape!=-1]
+            list_i, list_s, list_w = [], [], []
+            for i in np.arange(0.01, 0.31, 0.01):
+                p_threshold_s = i
+                p_threshold_w = i
+                num_s = int(len(s_no_minus1)*p_threshold_s)
+                num_w = int(len(w_no_minus1)*p_threshold_w)
+                s_thresholds = s_no_minus1[num_s-1]
+                w_thresholds = w_no_minus1[num_w-1]
+                masked_sCF = set_axes( MV.masked_less(sCF,s_thresholds)*0.+1 )
+                masked_wCF = set_axes( MV.masked_less(wCF,w_thresholds)*0.+1 )
+                s_avg_tmp = cdutil.averager(masked_sCF*sCF,axis='yx')
+                w_avg_tmp = cdutil.averager(masked_wCF*wCF,axis='yx')
+                list_i.append( round(i, 2) )
+                list_s.append( float(s_avg_tmp) )
+                list_w.append( float(w_avg_tmp) )
+                print (i, s_avg_tmp, w_avg_tmp) 
+
+            summary_array = np.zeros([3, len(list_i)])
+            summary_array[0] = list_i
+            summary_array[1] = list_s
+            summary_array[2] = list_w
+            # np.savetxt('Sara_list.csv', summary_array.T, delimiter=',', fmt='%.3f')
+            # print (summary_array) 
+            stop 
+
+
+
+    def make_grid_cell_selections(scf, wcf, region_mask, land_mask, selection_method, region_name, out_file):
+        scf_region = set_axes(scf * region_mask * land_mask)
+        wcf_region = set_axes(wcf * region_mask * land_mask)
+        s_mask_region, w_mask_region = select_CF(scf_region, wcf_region, selection_method)
+        # s_mask_region, w_mask_region = redo_loop(scf_region, wcf_region, selection_method)
+        s_mask_region.id = f'smask_{region_name}_mthd{selection_method}'
+        w_mask_region.id = f'wmask_{region_name}_mthd{selection_method}'
+        out_file.write(s_mask_region)
+        out_file.write(w_mask_region)
+        return s_mask_region, w_mask_region, scf_region * s_mask_region, wcf_region * w_mask_region
+
+
+
+
+    # ------------------------------------------------------------------------
+    # For US 
+    TwoLettersCode = ['US']
+    # fmask=cdms.open(f'selected_CtyMasks_ERA5.nc')
+    fmask=cdms.open(f'step1p1_selected_masks_US.nc')
+    g=cdms.open(f'step1p2_selected_USmask_outfile_ERA5.nc','w') 
+    results = {}
+    for region_name in TwoLettersCode:
+        print(f"REGION: {region_name}")
+        mask_region= fmask(f'mask_{region_name}')
+        results[region_name] = {}
+        for i in [ 1, 3]:
+            results[region_name][i] = make_grid_cell_selections(scf, wcf, mask_region, land_mask, i, region_name, g)
+    print("\nSaving masks to:")
+    print(g)
+    g.close()
+    fmask.close 
+
+main()
